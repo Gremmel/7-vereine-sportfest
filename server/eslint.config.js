cfe840d7a7645d8e838cdcfaa10d755c6e6cbdb2
MCH <christian.mayr@felss.com>
2024-10-15 12:00:42 +0200

    ui pfad hinzugefügt und aus server eslint vue entfernt

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import greRules from 'eslint-config-gre'; // Importiere dein Regelset
 import pluginJs from '@eslint/js';
-import pluginVue from 'eslint-plugin-vue';
 
 export default [
   { files: [ '**/*.{js,mjs,cjs,vue}' ]},
@@ -14,7 +13,6 @@ export default [
     }
   },
   pluginJs.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
   {
     rules: {
       ...greRules.rules // Füge die Regeln aus deinem Regelset hinzu
